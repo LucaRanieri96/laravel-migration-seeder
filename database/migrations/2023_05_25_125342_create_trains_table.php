2c759ffca5547e6ae105c766fce81326dc52901f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-25 15:12:22 +0200

    Fix attribute unsigned not required

--- database/migrations/2023_05_25_125342_create_trains_table.php
+++ database/migrations/2023_05_25_125342_create_trains_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda')->unsigned();
-            $table->string('stazione_partenza')->unsigned();
-            $table->string('stazione_arrivo')->unsigned();
-            $table->time('orario_partenza')->unsigned();
-            $table->time('orario_arrivo')->unsigned();
-            $table->string('codice_treno')->unsigned();
-            $table->integer('numero_carrozze')->nullable();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno');
+            $table->unsignedInteger('numero_carrozze')->nullable();
             $table->boolean('in_orario')->default(true);
             $table->boolean('cancellato')->default(false);
             $table->timestamps();
