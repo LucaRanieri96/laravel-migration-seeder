c122cb79e22369fe237c297c0f49dad7c3747836
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-25 14:52:59 +0200

    Add PageController and route

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        //dd(Train::all());
+        return view('home');
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+
+@section('content')
+<h1>Trains</h1>
+@endsection
