b2dbff1e09659474436d227e1efd65c7abbe1c49
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-25 17:14:32 +0200

    Add While cicle to fix values in TrainsSeeder

--- database/seeders/TrainsSeeder.php
+++ database/seeders/TrainsSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Train;
+use App\Models\Trains;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,25 +16,30 @@ class TrainsSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) {
-            $train = new Train();
+        for ($i = 0; $i < 20; $i++) {
+            $train = new Trains();
             $train->azienda = $faker->randomElement(['Trenitalia', 'Italo']);
 
             $train->stazione_partenza = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
             $stazionePartenza = $train->stazione_partenza;
-            while ($stazionePartenza !== $train->stazione_arrivo) {
-                $train->stazione_arrivo = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
+            $stazioneArrivo = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
+            while ($stazionePartenza == $stazioneArrivo) {
+                $stazioneArrivo = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
             }
-            
+            $train->stazione_arrivo = $stazioneArrivo;
+
             $train->orario_partenza = $faker->time();
             $orarioPartenza = $train->orario_partenza;
 
-            while ($orarioPartenza < $train->orario_arrivo) {
-                $train->orario_arrivo = $faker->time();
+            $orarioDiArrivo = $faker->time();
+            while ($orarioPartenza > $orarioDiArrivo) {
+                $orarioDiArrivo = $faker->time();
             }
+            $train->orario_arrivo = $orarioDiArrivo;
             
             $train->codice_treno = $faker->numberBetween(10000, 99999);
             $train->numero_carrozze = $faker->numberBetween(6, 25);
+            
             $train->in_orario = $faker->boolean();
             $train->cancellato = $faker->boolean();
             $train->save();
