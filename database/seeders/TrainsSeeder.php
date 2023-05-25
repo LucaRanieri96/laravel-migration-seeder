2743308bcd1f7845261118c9d4f69413256535a8
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-25 15:43:09 +0200

    Add TrainsSeeder content and send to db

--- database/seeders/TrainsSeeder.php
+++ database/seeders/TrainsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,20 @@ class TrainsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->randomElement(['Trenitalia', 'Italo']);
+            $train->stazione_partenza = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
+            $train->stazione_arrivo = $faker->randomElement(['Firenze', 'Arezzo', 'Grosseto', 'Livorno', 'Lucca', 'Massa-Carrara', 'Pisa', 'Pistoia', 'Prato', 'Siena']);
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->numberBetween(10000, 99999);
+            $train->numero_carrozze = $faker->numberBetween(6, 25);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
